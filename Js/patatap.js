bd677dfeceeace30ac33f7a7b2f35b317b494ab4
sebchiarelli <47152370+sebchiarelli@users.noreply.github.com>
2020-06-16 18:44:38 +0200

    Add files via upload

---
+++ Js/patatap.js
@@ -0,0 +1,15 @@
+//var test = 200;
+
+//span = document.querySelectorAll("script");
+//var ciclenumber = 0;
+
+//span.innerHTML = "var myCircle" + circleNumber + "= new Path+Circle(new Point(" + heightValue + ", " + witdhValue + "), 10);myCircle" + circleNumber + ".fillColor = 'red';"
+
+/*for(circleNumber = 0; circleNumber < 99; circleNumber++){
+    var circleHeight = 0;
+    var circleWitdh = 0;
+var shit[circleNumber] = new Path.Circle(new Point(circleHeight, circleWitdh), 50);myCircle.fillColor = 'yellow';";
+    span.innerHTML = ;
+    circleHeight++;
+    circleWitdh++;
+};*/
